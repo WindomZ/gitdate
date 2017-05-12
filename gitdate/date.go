4352807522ef34f9e33004a26c4b74393f5ad92a
WindomZ <git.windomz@gmail.com>
2017-05-12 08:21:06 +0800

    style: organize codes

--- gitdate/date.go
+++ gitdate/date.go
@@ -2,41 +2,12 @@ package gitdate
 
 import (
 	"errors"
-	"github.com/WindomZ/go-commander"
 	"regexp"
 	"strings"
 	"time"
 )
 
 const DATE_FORMAT string = time.RFC3339
-
-var DateArgvAction = func(c commander.Context) error {
-	date, ok := c.GetString("<date>")
-	if !ok || len(date) == 0 {
-		return errors.New("ERROR: <date> should not be empty.")
-	}
-
-	offset, err := formatDate(date)
-	if err != nil {
-		return err
-	}
-
-	if c.Contain("--day") || c.Contain("--month") || c.Contain("--year") {
-		offset = offset.AddDate(c.MustInt("--year"), c.MustInt("--month"), c.MustInt("--day"))
-	}
-	if i := c.MustInt("--hour"); i != 0 {
-		offset = offset.Add(time.Duration(i) * time.Hour)
-	}
-	if i := c.MustInt("--minute"); i != 0 {
-		offset = offset.Add(time.Duration(i) * time.Minute)
-	}
-
-	if offset.Equal(time.Now()) {
-		return nil
-	}
-
-	return execCommandGitDate(offset.Unix())
-}
 
 func formatDate(date string) (time.Time, error) {
 	var _time = time.Now()
